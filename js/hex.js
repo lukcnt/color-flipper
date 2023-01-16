d514e6358e7229bc8c615d68c31d018307663f0c
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-01-16 00:32:14 -0300

    Create a function that chagnes randomly the hex color of the page background

--- js/hex.js
+++ js/hex.js
@@ -1,1 +1,17 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const button = document.getElementById("button");
+const color = document.querySelector(".color");
+
+button.addEventListener("click", function() {
+    let hexColor = "#";
+    for (let index = 0; index < 6; index++){
+        hexColor += hex[getRandomNumber()];
+    }
+
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
